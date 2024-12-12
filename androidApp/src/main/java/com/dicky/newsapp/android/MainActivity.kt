86be99e3eda9e4ef7324249c63ef2c15d4e1a39e
Dicky59 <pekkaarvolaine@gmail.com>
2024-12-12 20:12:48 +0200

    Navigation working

--- androidApp/src/main/java/com/dicky/newsapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/dicky/newsapp/android/MainActivity.kt
@@ -5,13 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.dicky.newsapp.Platform
-import com.dicky.newsapp.android.screens.AboutScreen
-import com.dicky.newsapp.android.screens.ArticlesScreen
 import com.dicky.newsapp.articles.ArticlesViewModel
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         val articlesViewModel : ArticlesViewModel by viewModels()
 
-
         setContent {
             MyApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArticlesScreen(articlesViewModel = articlesViewModel)
+                    AppScaffold(articlesViewModel = articlesViewModel)
                 }
             }
         }
     }
 }
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        AboutScreen()
-    }
-}
